3ca64c088811be0e41980e9b6b6ed6f7ba6b26f6
Thomas <ammerlaan.thomas@gmail.com>
2021-11-16 11:57:54 +0100

    input and labels

--- mpl_squares.py
+++ mpl_squares.py
@@ -3,7 +3,24 @@ import matplotlib.pyplot as plt
 
 
 # plotting a line
-squares = [1, 4, 8, 16, 25]
+
+values = []
+squares = []
+for x in range(1,8):
+    values.append(x)
+    squares.append(x**2)
+
+# Create a figure 'fig' (matplotlib window) containing a plot 'ax'
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(values, squares, linewidth=2)
+
+# set title, and axis-labels
+ax.set_title("Square numbers", fontsize=24)
+ax.set_xlabel("Value (x)", fontsize=14)
+ax.set_ylabel("Square value (x²)", fontsize=14)
+
+# Set size of tick labels
+ax.tick_params(axis="both", labelsize=14)
+
+
 plt.show()
