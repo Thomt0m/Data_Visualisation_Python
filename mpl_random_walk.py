57f9f084553ca1d4ad4c929efc3f009808c496b7
Thomas <ammerlaan.thomas@gmail.com>
2021-11-16 17:09:56 +0100

    random walk refactor

--- mpl_random_walk.py
+++ mpl_random_walk.py
@@ -1,8 +1,6 @@
 import matplotlib.pyplot as plt
 from random import choice
 
-import numpy
-
 
 
 class RandomWalk:
@@ -12,10 +10,7 @@ class RandomWalk:
         """Initialise, set base attributes of a walk"""
 
         self.num_points = num_points
-
-        # Starting point (0, 0)
-        self.x_values = [0]
-        self.y_values = [0]
+        self._reset_walk()
 
 
     def fill_walk(self, num_points:int = -1, min_step_size:int = 0, max_step_size:int = 5):
@@ -24,22 +19,13 @@ class RandomWalk:
         # If no value was given for num_points default to the value set by constructor (self.num_points)
         if num_points <= 0: num_points = self.num_points
 
-        # Clear any previous walks
-        self.x_values.clear()
-        self.y_values.clear()
-        # Start at (0, 0)
-        self.x_values = [0]
-        self.y_values = [0]
+        # Clear any previous walk
+        self._reset_walk()
 
         while len(self.x_values) < num_points:
-
-            x_direction = choice([1, -1])
-            x_distance = choice(range(min_step_size, max_step_size))
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice(range(min_step_size, max_step_size))
-            y_step = y_direction * y_distance
+            
+            x_step = self._get_step(min_step_size, max_step_size)
+            y_step = self._get_step(min_step_size, max_step_size)
 
             # Discard steps that go nowhere
             if x_step == 0 and y_step == 0: continue
@@ -48,12 +34,36 @@ class RandomWalk:
             self.y_values.append(self.y_values[-1] + y_step)
 
 
+    def _get_step(self, min_step_size, max_step_size) -> int:
+        """Generate a step in the walk"""
+        direction = choice([1, -1])
+        distance = choice(range(min_step_size, max_step_size))
+        return direction * distance
+
+    
+    def _reset_walk(self):
+        """Reset self.x_values and self.y_values"""
+        # Start at (0, 0)
+        self.x_values = [0]
+        self.y_values = [0]
+
+
+
 
 
 rw = RandomWalk()
-rw.fill_walk()
+rw.fill_walk(50000)
 
-plt.scatter(rw.x_values, rw.y_values, c=range(len(rw.x_values)), cmap=plt.cm.viridis, s=6)
-plt.scatter(0, 0, color=(0.2, 1, 0.2), s=20)
-plt.scatter(rw.x_values[-1], rw.y_values[-1], color=(1, 0.2, 0.2), s=20)
+fig, ax = plt.subplots(figsize=(16, 9))
+
+ax.scatter(rw.x_values, rw.y_values, c=range(len(rw.x_values)), cmap=plt.cm.viridis, s=1)
+ax.scatter(0, 0, color=(0.2, 1, 0.2), s=20)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], color=(1, 0.2, 0.2), s=20)
+
+ax.set_title("Random walk", fontsize=16)
+
+# Remove both axes
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
 plt.show()
