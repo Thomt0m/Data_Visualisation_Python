4267c0d30cedc92706a2554ccd7d33465b6c884a
Thomas <ammerlaan.thomas@gmail.com>
2021-11-17 15:03:31 +0100

    rolling multiple dice of varying sizes

--- plotly_die_rolling.py
+++ plotly_die_rolling.py
@@ -21,20 +21,37 @@ class Die:
         """Roll the die"""
         return randint(1, self.sides)
 
-    def roll_multiple(self, roll_count) -> list:
+
+    def roll(self, sides) -> int:
+        """Roll the die, with a specified number of sides"""
+        return randint(1, sides)
+
+
+    def roll_multiple(self, roll_count:int) -> list:
         """Roll the die a specified number of times"""
         results = []
         for x in range(roll_count):
             results.append(self.roll())
         return results
 
-    def roll_multiple(self, roll_count, die_count) -> list:
+
+    def roll_multiple(self, roll_count:int, die_count:int) -> list:
         """Roll a specified number of times with the specified number of dice"""
         results = []
         for roll in range(roll_count):
             results.append(0)
             for die in range(die_count):
                 results[-1] += self.roll()
+        return results
+
+
+    def roll_multiple(self, roll_count:int, dice:list = []) -> list:
+        """Roll a specified number of times with the specified number of dice, each with the specified number of sides"""
+        results = []
+        for roll in range(roll_count):
+            results.append(0)
+            for die in range(len(dice)):
+                results[-1] += self.roll(dice[die])
         return results
 
 
@@ -47,24 +64,24 @@ class Die:
 
 # ---- Input values ----
 number_of_rolls = 1000
-die_sides = 6
-number_of_dice = 2
+die_sides_default = 6
+dice = [6, 10]
 
 
 
 
-die = Die(die_sides)
+die = Die(die_sides_default)
 # Generate some data
-results = die.roll_multiple(number_of_rolls, number_of_dice)
+results = die.roll_multiple(number_of_rolls, dice)
 
 # Analyse the result
 frequencies = []
-for value in range(number_of_dice, (die.sides * number_of_dice) + 1):
+for value in range(len(dice), (sum(dice)) + 1):
     frequencies.append(results.count(value))
 
 
 # Visualise the result
-x_values = list(range(number_of_dice, (die.sides * number_of_dice) + 1))
+x_values = list(range(len(dice), sum(dice) + 1))
 
 fig = pgo.Figure([pgo.Bar(x=x_values, y=frequencies)])
 fig.update_layout(title_text=f'Die rolls ({number_of_rolls}x)')
